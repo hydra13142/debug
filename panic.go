0346e6289dad8a9d2c1c3f33da7a4229644c81a3
liuzhixi <liudiwu@gmail.com>
2017-06-20 01:13:21 +0800

    Signed-off-by: liuzhixi <liudiwu@gmail.com>

--- panic.go
+++ panic.go
@@ -11,6 +11,7 @@ func Catch() {
 		fmt.Println(err)
 	}
 }
+
 func CatchAndGoexit() {
 	if err := recover(); err != nil {
 		fmt.Println(err)
@@ -45,4 +46,30 @@ func PanicAssist(ok bool) {
 			panic("Assist failed\n")
 		}
 	}
+}
+
+func PrintError(err error) bool {
+	if err != nil {
+		_, file, line, ok := runtime.Caller(1)
+		if ok {
+			fmt.Printf("%s: line %d:\n%v", file, line, err)
+		} else {
+			fmt.Print(err)
+		}
+		return true
+	}
+	return false
+}
+
+func PrintlnError(err error) bool {
+	if err != nil {
+		_, file, line, ok := runtime.Caller(1)
+		if ok {
+			fmt.Printf("%s: line %d:\n%v\n", file, line, err)
+		} else {
+			fmt.Println(err)
+		}
+		return true
+	}
+	return false
 }
